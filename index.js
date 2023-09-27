5fd9cb61f2fec3cf2271fa9c1f92ec8a9b73a53f
aminafarah <amishkamohamed563@gmail.com>
2023-09-28 01:44:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(entry => entry.result === "W");
+    if (win) {
+      return win.year;
+    }
+    // If no win object was found, return undefined
+    return undefined;
+  }
+  
